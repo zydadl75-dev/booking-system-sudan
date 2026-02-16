bc4e34789a664e3cf5699f4e20242abe33c27942
zydadl75-dev <zydadl75@gmail.com>
2026-02-16 14:46:55 -0800

    Add footer to footer.php with copyright notice

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+</div> <footer class="text-center mt-5 py-4 text-muted border-top">
+    © 2026 جميع الحقوق محفوظة - نظام الحجوزات
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
