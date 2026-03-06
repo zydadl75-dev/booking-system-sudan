c3539c223db908e7934b7a6229100cecb8a47d63
zydadl75-dev <zydadl75@gmail.com>
2026-03-06 11:20:00 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost"; $user = "root"; $pass = ""; $db = "booking_system";
+$host = "sql301.infinityfree.com"; $user = "if0_40459700"; $pass = "96P21XgLltXX8"; $db = "if0_40459700_hospital_db";
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) { die("خطأ في الاتصال: " . $e->getMessage()); }
 ?>
